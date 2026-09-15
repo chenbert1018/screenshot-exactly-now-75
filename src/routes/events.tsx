3687fda8ff6e1b3eaddb04d27f1c37b54a3f4ac8
chenbert1018 <chenbert97@gmail.com>
2026-09-15 17:07:44 +0800

    fix: preserve fan weather on quick event edit

--- src/routes/events.tsx
+++ src/routes/events.tsx
@@ -263,6 +263,10 @@ function EventsPage() {
             type: detail.type,
             date,
             note: detail.note,
+            locationName: detail.locationName ?? "",
+            city: detail.city ?? "",
+            weatherEnabled: Boolean(detail.weatherEnabled),
+            weatherTone: detail.weatherTone ?? "SUNSHINE",
           });
           setDetailId(null);
         }}
